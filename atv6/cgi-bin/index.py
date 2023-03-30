266464b480d5a72053088db966bcdfe41f4994e4
Mateus Fernandes Santos <ferrmathh2@gmail.com>
2023-03-30 15:26:46 -0300

    commit

--- atv6/cgi-bin/index.py
+++ atv6/cgi-bin/index.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 import os
 from urllib.parse import parse_qs
 import json  
@@ -7,8 +5,8 @@ import datetime
 
 qs = os.environ["QUERY_STRING"]
 list = parse_qs(qs, encoding="latin-1")
-var = list["nome"][0]
-var2 = list["mensagem"][0]
+var = list["name"][0]
+var2 = list["message"][0]
 with open('banco.json', 'r') as openfile: 
     json_object = json.load(openfile) 
 json_object["arquivos"].append({"nome":var,"data":str(datetime.datetime.now()),"mensagem":var2})
@@ -18,7 +16,7 @@ with open('banco.json', 'w') as outfile:
 print("Content-type: text/html")
 print()
 print("<html><head><title>Exemplo CGI</title></head><body>")
-print("QUERY_STRING = '" + qs + "'<br>")
+print("<div style='display: flex; width: 100%; flex-direction: column; align-items: center; justify-content: center; gap: 16px;'>")
 for arquivo in json_object["arquivos"]:
-    print("Nome = '" + arquivo['nome']+"'" + "Data =" + arquivo["data"] + "<br>" +"   Mensagem =" + arquivo["mensagem"] + "<br>")
-print("</body></html>")
+    print("Nome = '" + arquivo['nome']+"'" + "<br>   Data =" + arquivo["data"] + "<br>" +"   Mensagem =" + arquivo["mensagem"] + "<br><br><br>")
+print("</div></body></html>")
